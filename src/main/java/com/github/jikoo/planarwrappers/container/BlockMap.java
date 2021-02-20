3f7fc6e2a0bf184aacc640fe168b1996962951ce
Jikoo <jikoo.games@gmail.com>
2021-02-20 10:32:20 -0500

    Add BlockMap

---
+++ src/main/java/com/github/jikoo/planarwrappers/container/BlockMap.java
@@ -0,0 +1,202 @@
+package com.github.jikoo.planarwrappers.container;
+
+import com.github.jikoo.planarwrappers.util.Coords;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import org.bukkit.Chunk;
+import org.bukkit.block.Block;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * A more performant expression of a {@code Map<Block, ?>}. Also supports chunk-based operations.
+ *
+ * @param <V> the type of value stored
+ */
+public class BlockMap<V> {
+
+  private final Map<String, SortedMap<Integer, SortedMap<Integer, Map<Integer, V>>>> serverMap =
+      FastMap.obj2Obj();
+
+  /**
+   * Add a mapping for a {@link Block}.
+   *
+   * @param block the key used to map the value
+   * @param value the value to be stored using the key
+   * @return the previously stored value or {@code null} if no value was present
+   */
+  public @Nullable V put(@NotNull Block block, @Nullable V value) {
+    SortedMap<Integer, SortedMap<Integer, Map<Integer, V>>> worldMap =
+        serverMap.computeIfAbsent(block.getWorld().getName(), k -> FastMap.int2ObjTree());
+    SortedMap<Integer, Map<Integer, V>> blockXMap =
+        worldMap.computeIfAbsent(block.getX(), k -> FastMap.int2ObjTree());
+    Map<Integer, V> blockZMap =
+        blockXMap.computeIfAbsent(block.getZ(), blockZ -> FastMap.int2Obj());
+
+    return blockZMap.put(block.getY(), value);
+  }
+
+  /**
+   * Get a stored mapping for a {@link Block}.
+   *
+   * @param block the key used to map the value
+   * @return the stored value or {@code null} if no value is present
+   */
+  public @Nullable V get(@NotNull Block block) {
+    SortedMap<Integer, SortedMap<Integer, Map<Integer, V>>> xzyMap =
+        serverMap.get(block.getWorld().getName());
+    if (xzyMap == null) {
+      return null;
+    }
+
+    SortedMap<Integer, Map<Integer, V>> zyMap = xzyMap.get(block.getX());
+    if (zyMap == null) {
+      return null;
+    }
+
+    Map<Integer, V> yMap = zyMap.get(block.getZ());
+    if (yMap == null) {
+      return null;
+    }
+
+    return yMap.get(block.getY());
+  }
+
+  /**
+   * Get all stored mappings for a {@link Chunk}.
+   *
+   * @param chunk the {@code Chunk} of blocks to retrieve
+   * @return the stored values
+   */
+  public @NotNull Collection<V> get(@NotNull Chunk chunk) {
+    return get(chunk.getWorld().getName(), chunk.getX(), chunk.getZ());
+  }
+
+  /**
+   * Get all stored mappings for the chunk coordinates in the world named.
+   *
+   * <p>Note that this method accepts {@link Chunk} coordinates, not block coordinates! If
+   * necessary, convert with the {@link Coords} utility.
+   *
+   * @param world the name of the world
+   * @param chunkX the chunk X coordinate
+   * @param chunkZ the chunk Z coordinate
+   * @return the stored values
+   */
+  public @NotNull Collection<V> get(@NotNull String world, int chunkX, int chunkZ) {
+    SortedMap<Integer, SortedMap<Integer, Map<Integer, V>>> xzyMap = serverMap.get(world);
+    if (xzyMap == null) {
+      return Collections.emptyList();
+    }
+
+    int blockXMin = Coords.chunkToBlock(chunkX);
+    // Submap fetching is high boundary exclusive, shift responsibility for chunk size to Coords.
+    int blockXMax = Coords.chunkToBlock(chunkX + 1);
+    SortedMap<Integer, SortedMap<Integer, Map<Integer, V>>> zySubMap =
+        xzyMap.subMap(blockXMin, blockXMax);
+    if (zySubMap.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    List<V> values = new ArrayList<>();
+    int blockZMin = Coords.chunkToBlock(chunkZ);
+    int blockZMax = Coords.chunkToBlock(chunkZ + 1);
+    for (Map.Entry<Integer, SortedMap<Integer, Map<Integer, V>>> zyEntry : zySubMap.entrySet()) {
+      SortedMap<Integer, Map<Integer, V>> ySubMap = zyEntry.getValue().subMap(blockZMin, blockZMax);
+
+      for (Map<Integer, V> blockYMap : ySubMap.values()) {
+        values.addAll(blockYMap.values());
+      }
+    }
+
+    return values;
+  }
+
+  /**
+   * Remove a mapping for a {@link Block}.
+   *
+   * @param block the key used to map the value
+   * @return the previously stored value or {@code null} if no value was present
+   */
+  public @Nullable V remove(@NotNull Block block) {
+    SortedMap<Integer, SortedMap<Integer, Map<Integer, V>>> xzyMap =
+        serverMap.get(block.getWorld().getName());
+    if (xzyMap == null) {
+      return null;
+    }
+
+    SortedMap<Integer, Map<Integer, V>> zyMap = xzyMap.get(block.getX());
+    if (zyMap == null) {
+      return null;
+    }
+
+    Map<Integer, V> yMap = zyMap.get(block.getZ());
+    if (yMap == null) {
+      return null;
+    }
+
+    return yMap.remove(block.getY());
+  }
+
+  /**
+   * Remove all stored mappings for a {@link Chunk}.
+   *
+   * @param chunk the {@code Chunk} of blocks to remove
+   * @return the previously stored values
+   */
+  public @NotNull Collection<V> remove(@NotNull Chunk chunk) {
+    return remove(chunk.getWorld().getName(), chunk.getX(), chunk.getZ());
+  }
+
+  /**
+   * Remove all stored mappings for the chunk coordinates in the world named.
+   *
+   * <p>Note that this method accepts {@link Chunk} coordinates, not block coordinates! If
+   * necessary, convert with the {@link Coords} utility.
+   *
+   * @param world the name of the world
+   * @param chunkX the chunk X coordinate
+   * @param chunkZ the chunk Z coordinate
+   * @return the previously stored values
+   */
+  public @NotNull Collection<V> remove(@NotNull String world, int chunkX, int chunkZ) {
+    SortedMap<Integer, SortedMap<Integer, Map<Integer, V>>> xzyMap = serverMap.get(world);
+    if (xzyMap == null) {
+      return Collections.emptyList();
+    }
+
+    int blockXMin = Coords.chunkToBlock(chunkX);
+    SortedMap<Integer, SortedMap<Integer, Map<Integer, V>>> zySubMap =
+        xzyMap.subMap(blockXMin, blockXMin + 16);
+    if (zySubMap.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    List<V> values = new ArrayList<>();
+    int blockZMin = Coords.chunkToBlock(chunkZ);
+
+    for (Iterator<SortedMap<Integer, Map<Integer, V>>> blockXIterator =
+            zySubMap.values().iterator();
+        blockXIterator.hasNext(); ) {
+      SortedMap<Integer, Map<Integer, V>> zyValue = blockXIterator.next();
+      SortedMap<Integer, Map<Integer, V>> ySubMap = zyValue.subMap(blockZMin, blockZMin + 16);
+
+      for (Iterator<Map<Integer, V>> yIterator = ySubMap.values().iterator();
+          yIterator.hasNext(); ) {
+        values.addAll(yIterator.next().values());
+        yIterator.remove();
+      }
+
+      if (zyValue.isEmpty()) {
+        blockXIterator.remove();
+      }
+    }
+
+    return values;
+  }
+}
---
+++ src/main/java/com/github/jikoo/planarwrappers/container/FastMap.java
@@ -0,0 +1,109 @@
+package com.github.jikoo.planarwrappers.container;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.function.Supplier;
+
+/**
+ * A utility for creating FastUtil maps.
+ *
+ * <p>While Craftbukkit and Paper both shade FastUtil, CB relocates FastUtil and Paper does not. The
+ * performance gains of using FastUtil's maps are worth the hit of fumbling around a bit initially
+ * to figure out which is present.
+ */
+@SuppressWarnings("unchecked")
+class FastMap {
+
+  private static Class<Map<?, ?>> obj2Obj;
+  private static Class<SortedMap<Integer, ?>> int2ObjTree;
+  private static Class<Map<Integer, ?>> int2Obj;
+
+  private FastMap() {}
+
+  /**
+   * Create an {@code Object2ObjectOpenHashMap} if the class can be found.
+   *
+   * <p>If FastUtil cannot be located, provides a {@code HashMap} instead.
+   *
+   * @param <K> the type of key
+   * @param <V> the type of value
+   * @return the Map created
+   */
+  static <K, V> Map<K, V> obj2Obj() {
+    if (obj2Obj == null) {
+      obj2Obj = newFast("it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap", HashMap.class);
+    }
+
+    return (Map<K, V>) instanceOr(obj2Obj, HashMap::new);
+  }
+
+  /**
+   * Create an {@code Int2ObjectOpenHashMap} if the class can be found.
+   *
+   * <p>If FastUtil cannot be located, provides a {@code HashMap} instead.
+   *
+   * @param <V> the type of value
+   * @return the Map created
+   */
+  static <V> Map<Integer, V> int2Obj() {
+    if (int2Obj == null) {
+      int2Obj = newFast("it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap", HashMap.class);
+    }
+
+    return (Map<Integer, V>) instanceOr(int2Obj, HashMap::new);
+  }
+
+  /**
+   * Create an {@code Int2ObjectRBTreeMap} if the class can be found.
+   *
+   * <p>If FastUtil cannot be located, provides a {@code HashMap} instead.
+   *
+   * @param <V> the type of value
+   * @return the Map created
+   */
+  static <V> SortedMap<Integer, V> int2ObjTree() {
+    if (int2ObjTree == null) {
+      int2ObjTree = newFast("it.unimi.dsi.fastutil.ints.Int2ObjectRBTreeMap", TreeMap.class);
+    }
+
+    return (SortedMap<Integer, V>) instanceOr(int2ObjTree, TreeMap::new);
+  }
+
+  private static <T extends Map<?, ?>, V extends T> Class<T> newFast(
+      String className, Class<V> defaultClazz) {
+    if (Modifier.isAbstract(defaultClazz.getModifiers())) {
+      throw new IllegalArgumentException(
+          "Cannot provide a default implementation that cannot be instantiated!");
+    }
+    try {
+      // CB/Spigot provides FastUtil but relocates it.
+      return (Class<T>) Class.forName("org.bukkit.craftbukkit.libs." + className);
+    } catch (ClassNotFoundException e) {
+      // Not relocated by CB, move on.
+    }
+    try {
+      // Paper removes FastUtil relocation.
+      return (Class<T>) Class.forName(className);
+    } catch (ClassNotFoundException e) {
+      // Not present at all? Fall through to provided implementation.
+    }
+    // Hack around compilation error
+    return (Class<T>) defaultClazz;
+  }
+
+  private static <T> T instanceOr(Class<? extends T> clazz, Supplier<T> supplier) {
+    try {
+      return clazz.getConstructor().newInstance();
+    } catch (InstantiationException
+        | InvocationTargetException
+        | IllegalAccessException
+        | NoSuchMethodException e) {
+      // Fall through to supplier.
+      return supplier.get();
+    }
+  }
+}
