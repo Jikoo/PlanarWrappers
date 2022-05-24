bf8b638dba19da88e20290c94a0cab2f78196974
Jikoo <jikoo.games@gmail.com>
2022-05-24 11:10:05 -0400

    Add CachingSupplier

---
+++ src/main/java/com/github/jikoo/planarwrappers/tuple/CachingSupplier.java
@@ -0,0 +1,47 @@
+package com.github.jikoo.planarwrappers.tuple;
+
+import java.time.Clock;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+public class CachingSupplier<T> implements Supplier<T> {
+
+  private final @NotNull Supplier<T> supplier;
+  private final long cacheDuration;
+  private final @NotNull Clock clock;
+  private T value;
+  private long lastUpdate = Long.MIN_VALUE;
+
+  public CachingSupplier(
+      @NotNull Supplier<T> supplier,
+      long duration,
+      @NotNull TimeUnit timeUnit) {
+    this(supplier, duration, timeUnit, Clock.systemUTC());
+  }
+
+  public CachingSupplier(
+      @NotNull Supplier<T> supplier,
+      @Range(from = 1, to = Long.MAX_VALUE) long duration,
+      @NotNull TimeUnit timeUnit,
+      @NotNull Clock clock) {
+    this.supplier = supplier;
+    this.cacheDuration = TimeUnit.MILLISECONDS.convert(duration, timeUnit);
+    this.clock = clock;
+  }
+
+  /**
+   * Returns the cached value, refreshing as required.
+   *
+   * @return the cached value
+   */
+  public T get() {
+    if (lastUpdate <= clock.millis() - cacheDuration) {
+      value = supplier.get();
+      lastUpdate = clock.millis();
+    }
+    return value;
+  }
+
+}
---
+++ src/test/java/com/github/jikoo/planarwrappers/tuple/CachingSupplierTest.java
@@ -0,0 +1,100 @@
+package com.github.jikoo.planarwrappers.tuple;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.github.jikoo.planarwrappers.util.MockClock;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("Feature: Supplier that caches the returned value")
+@TestInstance(Lifecycle.PER_METHOD)
+class CachingSupplierTest {
+
+  @DisplayName("Ensure that values are obtained as expected.")
+  @ParameterizedTest
+  @MethodSource("getBooleans")
+  void testGet(Boolean value) {
+    Supplier<Boolean> cache = new CachingSupplier<>(() -> value, 5, TimeUnit.MILLISECONDS);
+    assertThat("Value must be supplied as expected", cache.get(), is(value));
+  }
+
+  @DisplayName("Ensure that values are cached.")
+  @Test
+  void testCachedValueUsed() {
+    Supplier<Boolean> singleUseSupplier = new SingleUseSupplier<>(true);
+    Supplier<Boolean> cache = new CachingSupplier<>(singleUseSupplier, 5, TimeUnit.MILLISECONDS);
+
+    assertThat("Value must be supplied as expected", cache.get());
+    assertThrows(
+        IllegalStateException.class,
+        singleUseSupplier::get,
+        "Supplier may only be used once");
+    assertDoesNotThrow(cache::get, "Supplier must only be used once");
+  }
+
+  @DisplayName("Ensure that values are re-obtained after the cache period expires.")
+  @Test
+  void testCachedValueExpires() {
+    Supplier<Boolean> singleUseSupplier = new SingleUseSupplier<>(true);
+    MockClock clock = new MockClock();
+    Supplier<Boolean> cache = new CachingSupplier<>(
+        singleUseSupplier,
+        5,
+        TimeUnit.MILLISECONDS,
+        clock);
+    assertThat("Value must be supplied as expected", cache.get());
+    assertThrows(
+        IllegalStateException.class,
+        singleUseSupplier::get,
+        "Supplier may only be used once");
+    assertDoesNotThrow(cache::get, "Supplier must only be used once");
+
+    clock.add(5, TimeUnit.MILLISECONDS);
+    assertThrows(
+        IllegalStateException.class,
+        cache::get,
+        "Cache must expire after appropriate time period");
+  }
+
+  @Contract(pure = true)
+  private static @NotNull Collection<Boolean> getBooleans() {
+    return Arrays.asList(
+        Boolean.TRUE,
+        Boolean.FALSE,
+        null
+    );
+  }
+
+  private static class SingleUseSupplier<T> implements Supplier<T> {
+    private final T value;
+    private boolean used = false;
+
+    private SingleUseSupplier(T value) {
+      this.value = value;
+    }
+
+    @Override
+    public T get() {
+      if (used) {
+        throw new IllegalStateException("Cannot be called more than once!");
+      }
+      used = true;
+      return value;
+    }
+
+  }
+
+}
