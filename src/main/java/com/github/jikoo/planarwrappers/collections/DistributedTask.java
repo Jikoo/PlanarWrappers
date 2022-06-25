ab57c86355a38b4e800d451c6f75f52b7da17f22
Jikoo <jikoo.games@gmail.com>
2022-06-25 11:44:30 -0400

    Add DistributedTask for bucketing operations
    Not really well-optimized, but it does what I need it to for now.

---
+++ src/main/java/com/github/jikoo/planarwrappers/collections/DistributedTask.java
@@ -0,0 +1,135 @@
+package com.github.jikoo.planarwrappers.collections;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnmodifiableView;
+
+/**
+ * A system for dividing operations up across multiple ticks to reduce server load. The total period
+ * is used to form "buckets" of objects that are handled by a consumer each server tick.
+ *
+ * <p>This system is designed for tasks with a short overall period and many elements. It is not
+ * optimized for periods longer than a few seconds, and may perform poorly in those situations.
+ *
+ * <p>The backing collection is a {@link Set}. Duplicate elements will be ignored.
+ *
+ * @param <T> the type of object stored
+ */
+public class DistributedTask<T> {
+
+  private final Set<T> allContent = new HashSet<>();
+  private final @NotNull Set<T> @NotNull [] distributedContent;
+  private final @NotNull Consumer<Collection<T>> consumer;
+  private int taskId = -1;
+  private int currentIndex = 0;
+
+  /**
+   * Construct a new {@code DistributedTask}.
+   *
+   * @param period the total period
+   * @param periodUnit the {@link TimeUnit} of the period
+   * @param consumer the {@link Consumer} handling each bucket's contents
+   * @throws IllegalArgumentException if the period is under 100MS (2 server ticks)
+   */
+  @SuppressWarnings("unchecked")
+  public DistributedTask(
+      long period,
+      @NotNull TimeUnit periodUnit,
+      @NotNull Consumer<@UnmodifiableView Collection<T>> consumer) {
+    int totalTicks = (int) (TimeUnit.MILLISECONDS.convert(period, periodUnit) / 50);
+    if (totalTicks < 2) {
+      throw new IllegalArgumentException("Period must be 2 ticks (100 MS) or greater");
+    }
+
+    distributedContent = (Set<T>[]) Array.newInstance(allContent.getClass(), totalTicks);
+    for (int index = 0; index < distributedContent.length; ++index) {
+      distributedContent[index] = new HashSet<>();
+    }
+
+    this.consumer = consumer;
+  }
+
+  /**
+   * Add an element.
+   *
+   * @param content the element to add
+   */
+  public void add(@NotNull T content) {
+    if (!allContent.add(content)) {
+      return;
+    }
+
+    int lowestSize = Integer.MAX_VALUE;
+    int lowestIndex = 0;
+    for (int index = 0; index < distributedContent.length; ++index) {
+      int size = distributedContent[index].size();
+      if (size < lowestSize) {
+        lowestSize = size;
+        lowestIndex = index;
+      }
+    }
+
+    distributedContent[lowestIndex].add(content);
+  }
+
+  /**
+   * Remove an element.
+   *
+   * @param content the element to remove
+   */
+  public void remove(@NotNull T content) {
+    if (allContent.remove(content)) {
+      for (Set<T> contentPartition : distributedContent) {
+        if (contentPartition.remove(content)) {
+          break;
+        }
+      }
+    }
+  }
+
+  private void run() {
+    consumer.accept(Collections.unmodifiableSet(distributedContent[currentIndex]));
+    ++currentIndex;
+    if (currentIndex >= distributedContent.length) {
+      currentIndex = 0;
+    }
+  }
+
+  /**
+   * Schedule the task with the Bukkit scheduler.
+   *
+   * @param plugin the plugin scheduling the task
+   * @return the same task instance
+   */
+  @Contract("_ -> this")
+  public @NotNull DistributedTask<T> schedule(@NotNull Plugin plugin) {
+    if (taskId != -1) {
+      cancel(plugin);
+    }
+
+    taskId = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this::run, 1, 1);
+
+    return this;
+  }
+
+  /**
+   * Cancel the task scheduled with the Bukkit scheduler.
+   *
+   * @param plugin the plugin cancelling the task
+   */
+  public void cancel(@NotNull Plugin plugin) {
+    if (taskId != -1) {
+      plugin.getServer().getScheduler().cancelTask(taskId);
+      taskId = -1;
+    }
+  }
+
+}
---
+++ src/test/java/com/github/jikoo/planarwrappers/collections/DistributedTaskTest.java
@@ -0,0 +1,214 @@
+package com.github.jikoo.planarwrappers.collections;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import org.bukkit.Server;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.stubbing.Answer;
+
+@TestInstance(Lifecycle.PER_CLASS)
+class DistributedTaskTest {
+
+  private static final int TASK_BUCKETS = 5;
+
+  Plugin plugin;
+  Runnable runTask;
+
+  @BeforeAll
+  void beforeAll() {
+    plugin = mock(Plugin.class);
+
+    // Set up scheduler mock to pull DistributedTask#run as a runnable.
+    BukkitScheduler scheduler = mock(BukkitScheduler.class);
+    when(scheduler.scheduleSyncRepeatingTask(any(), any(Runnable.class), anyLong(), anyLong()))
+        .thenAnswer((Answer<Integer>) invocation -> {
+          runTask = invocation.getArgument(1);
+          return 2;
+        });
+    doNothing().when(scheduler).cancelTask(anyInt());
+
+    Server server = mock(Server.class);
+    doReturn(scheduler).when(server).getScheduler();
+
+    doReturn(server).when(plugin).getServer();
+  }
+
+  @AfterEach
+  void afterEach() {
+    //noinspection ConstantConditions
+    runTask = null;
+  }
+
+  @Contract("_ -> new")
+  private @NotNull DistributedTask<Object> createTask(Consumer<Collection<Object>> consumer) {
+    DistributedTask<Object> task = new DistributedTask<>(
+        TASK_BUCKETS * 50,
+        TimeUnit.MILLISECONDS,
+        consumer);
+    task.schedule(plugin);
+    return task;
+  }
+
+  private void tickAllBuckets() {
+    for (int i = 0; i < TASK_BUCKETS; ++i) {
+      runTask.run();
+    }
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = { -50, 0, 50 })
+  void testCreateUselessTask(int value) {
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new DistributedTask<>(value, TimeUnit.MILLISECONDS, objects -> {}),
+        "Distributed task with one bucket is useless.");
+  }
+
+  @Test
+  void testAddContent() {
+    List<Object> objects = Arrays.asList("test", "successful");
+    List<Object> handledObjects = new ArrayList<>();
+
+    DistributedTask<Object> task = createTask(handledObjects::addAll);
+    objects.forEach(task::add);
+
+    tickAllBuckets();
+
+    assertThat(handledObjects, hasSize(objects.size()));
+    assertThat(handledObjects, containsInAnyOrder(objects.toArray()));
+  }
+
+  @Test
+  void testDuplicateContentIgnored() {
+    List<Object> objects = Arrays.asList("test", "successful");
+    List<Object> handledObjects = new ArrayList<>();
+
+    DistributedTask<Object> task = createTask(handledObjects::addAll);
+    objects.forEach(task::add);
+    objects.forEach(task::add);
+
+    tickAllBuckets();
+
+    assertThat(handledObjects, hasSize(objects.size()));
+    assertThat(handledObjects, containsInAnyOrder(objects.toArray()));
+  }
+
+  @Test
+  void testRemoveContent() {
+    List<Object> objects = Arrays.asList("test", "successful");
+    assertThat(
+        "Must test with two or more objects to cover conditionals",
+        objects,
+        hasSize(greaterThan(1)));
+    List<Object> handledObjects = new ArrayList<>();
+
+    DistributedTask<Object> task = createTask(handledObjects::addAll);
+    objects.forEach(task::add);
+    objects.forEach(task::remove);
+    // Double remove to test conditional.
+    objects.forEach(task::remove);
+
+    tickAllBuckets();
+
+    assertThat(handledObjects, hasSize(0));
+
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = { 1, 2, 3, 10 })
+  void testContentDistribution(int elementsPerBucket) {
+    DistributedTask<Object> task = createTask(
+        objects ->
+            assertThat(
+                "Objects must be spread evenly across buckets",
+                objects,
+                hasSize(elementsPerBucket)));
+
+    for (int i = 0; i < elementsPerBucket * TASK_BUCKETS; ++i) {
+      task.add(i);
+    }
+
+    tickAllBuckets();
+  }
+
+  @Test
+  void testContentTicks() {
+    AtomicInteger timesCalled = new AtomicInteger();
+
+    DistributedTask<Object> task = createTask(
+        objects -> {
+          assertThat("Each bucket contains one element", objects, hasSize(1));
+          Object object = objects.stream().findFirst().orElse(null);
+          assertThat("Object must be an AtomicInteger", object, instanceOf(AtomicInteger.class));
+          AtomicInteger integer = (AtomicInteger) object;
+          assert integer != null; // Compiler doesn't realize instanceOf fails on null
+          assertThat(
+              "Times called must match expected",
+              integer.incrementAndGet(),
+              is(timesCalled.get()));
+        });
+
+    AtomicInteger[] atomicIntegerArray = new AtomicInteger[TASK_BUCKETS];
+    Arrays.setAll(atomicIntegerArray, ignoredIndex -> new AtomicInteger());
+
+    for (AtomicInteger atomicInteger : atomicIntegerArray) {
+      task.add(atomicInteger);
+    }
+
+    task.schedule(plugin);
+
+    while (timesCalled.incrementAndGet() < 5) {
+      tickAllBuckets();
+    }
+  }
+
+  @Test
+  void testUnscheduledCancel() {
+    new DistributedTask<>(100, TimeUnit.MILLISECONDS, ignored -> {}).cancel(plugin);
+  }
+
+  @Test
+  void testScheduleCancel() {
+    DistributedTask<Object> task = new DistributedTask<>(100, TimeUnit.MILLISECONDS, ignored -> {});
+    task.schedule(plugin);
+    task.cancel(plugin);
+  }
+
+  @Test
+  void testDuplicateSchedule() {
+    DistributedTask<Object> task = new DistributedTask<>(100, TimeUnit.MILLISECONDS, ignored -> {});
+    task.schedule(plugin);
+    task.schedule(plugin);
+  }
+
+}
