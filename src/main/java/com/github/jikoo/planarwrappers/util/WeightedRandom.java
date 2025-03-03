32a35c01ffd3184f4485af1fe728764f9a5ae446
Jikoo <jikoo.games@gmail.com>
2025-03-02 22:34:54 -0500

    Allow arbitrary ToIntFunction usage
    Allows usage with objects that have declared weights but do not extend WeightedRandom.Choice, i.e. enchantments.

--- src/main/java/com/github/jikoo/planarwrappers/util/WeightedRandom.java
+++ src/main/java/com/github/jikoo/planarwrappers/util/WeightedRandom.java
@@ -2,6 +2,7 @@ package com.github.jikoo.planarwrappers.util;
 
 import java.util.Collection;
 import java.util.Random;
+import java.util.function.ToIntFunction;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Range;
 
@@ -17,11 +18,12 @@ public final class WeightedRandom {
    *
    * @param random the Random to use
    * @param choices the choices
+   * @param toInt a conversion from a choice to an integer weight.
    * @param <T> the type of choice
    * @return the selected choice
    */
-  public static <T extends Choice> @NotNull T choose(Random random, Collection<T> choices) {
-    int choiceMax = sum(choices);
+  public static <T> @NotNull T choose(Random random, Collection<T> choices, ToIntFunction<T> toInt) {
+    int choiceMax = sum(choices, toInt);
 
     if (choiceMax <= 0) {
       throw new IllegalArgumentException("Must provide at least 1 choice with weight!");
@@ -30,7 +32,7 @@ public final class WeightedRandom {
     int chosen = random.nextInt(choiceMax);
 
     for (T choice : choices) {
-      chosen -= choice.getWeight();
+      chosen -= toInt.applyAsInt(choice);
       if (chosen <= 0) {
         return choice;
       }
@@ -40,8 +42,20 @@ public final class WeightedRandom {
         "Generated an index out of bounds with " + random.getClass().getName());
   }
 
-  private static int sum(@NotNull Collection<? extends Choice> choices) {
-    return choices.stream().mapToInt(Choice::getWeight).sum();
+  /**
+   * Choose an element from a collection of choices.
+   *
+   * @param random the Random to use
+   * @param choices the choices
+   * @param <T> the type of choice
+   * @return the selected choice
+   */
+  public static <T extends Choice> @NotNull T choose(Random random, Collection<T> choices) {
+    return choose(random, choices, Choice::getWeight);
+  }
+
+  private static <T> int sum(@NotNull Collection<T> choices, ToIntFunction<T> toInt) {
+    return choices.stream().mapToInt(toInt).sum();
   }
 
   /** A weighted random choice. Chance of selection is based on total choices and weights. */
--- src/test/java/com/github/jikoo/planarwrappers/util/WeightedRandomTest.java
+++ src/test/java/com/github/jikoo/planarwrappers/util/WeightedRandomTest.java
@@ -3,6 +3,7 @@ package com.github.jikoo.planarwrappers.util;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.github.jikoo.planarwrappers.util.WeightedRandom.Choice;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -23,6 +24,7 @@ class WeightedRandomTest {
     Collection<WeightedRandom.Choice> choices = Collections.emptyList();
 
     assertThrows(IllegalArgumentException.class, () -> WeightedRandom.choose(random, choices));
+    assertThrows(IllegalArgumentException.class, () -> WeightedRandom.choose(random, choices, Choice::getWeight));
   }
 
   @DisplayName("A minimum of 1 available element must have weight.")
@@ -32,6 +34,7 @@ class WeightedRandomTest {
     Collection<WeightedRandom.Choice> choices = Arrays.asList(() -> 0, () -> 0);
 
     assertThrows(IllegalArgumentException.class, () -> WeightedRandom.choose(random, choices));
+    assertThrows(IllegalArgumentException.class, () -> WeightedRandom.choose(random, choices, Choice::getWeight));
   }
 
   @DisplayName("Invalid random behavior should generate an exception.")
@@ -47,6 +50,7 @@ class WeightedRandomTest {
     Collection<WeightedRandom.Choice> choices = Arrays.asList(() -> 1, () -> 2, () -> 3);
 
     assertThrows(IllegalStateException.class, () -> WeightedRandom.choose(random, choices));
+    assertThrows(IllegalStateException.class, () -> WeightedRandom.choose(random, choices, Choice::getWeight));
   }
 
   @DisplayName("Valid choices should result in a valid selection.")
@@ -56,5 +60,6 @@ class WeightedRandomTest {
     Collection<WeightedRandom.Choice> choices = Arrays.asList(() -> 1, () -> 2, () -> 3);
 
     assertDoesNotThrow(() -> WeightedRandom.choose(random, choices));
+    assertDoesNotThrow(() -> WeightedRandom.choose(random, choices, Choice::getWeight));
   }
 }
